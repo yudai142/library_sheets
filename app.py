1f99fe2fcff762073561ad8033c08aa0290e59da
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-05-29 19:14:17 +0900

    削除処理作成

--- app.py
+++ app.py
@@ -45,3 +45,10 @@ def update(id):
     post.body = request.form.get('body')
     db.session.commit()
     return redirect ("/")
+
+@app.route("/<int:id>/delete", methods=['GET'])
+def delete(id):
+  post = Post.query.get(id)
+  db.session.delete(post)
+  db.session.commit()
+  return redirect ("/")
