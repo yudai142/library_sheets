6b262f6eb147be81e82f1e07f11b7983b0a49f4b
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-05-29 18:33:54 +0900

    新規作成・表示処理実装

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
+from crypt import methods
 from flask import Flask
+from flask import render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
 
-@app.route("/")
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String(300), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(pytz.timezone('Asia/Tokyo')))
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return "index page"
+  if request.method == 'GET':
+    posts = Post.query.all()
+    return render_template("index.html", posts=posts)
+
+@app.route("/create", methods=['GET', 'POST'])
+def create():
+  if request.method == 'POST':
+    title = request.form.get('title')
+    body = request.form.get('body')
+
+    post = Post(title=title, body=body)
+
+    db.session.add(post)
+    db.session.commit()
+    return redirect ("/")
+  else:
+    return render_template("create.html")
